96c87c8687b07042ab40792365143169b9291226
Joris Hartog <jorishartog@hotmail.com>
2022-09-29 20:13:42 +0200

    Add health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use warp::Filter;
+
+fn get_healthz() -> &'static str {
+    ""
+}
+
+#[tokio::main]
+async fn main() {
+    let get_routes = warp::get().and(warp::path("healthz").map(get_healthz));
+
+    warp::serve(get_routes).run(([127, 0, 0, 1], 8000)).await;
 }
